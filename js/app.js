8d0e6cb808d9fd4eb731bb2850ace55a5e2b18b8
MithunAbs <mithun.ss@assuretech.in>
2026-09-10 12:29:46 +0530

    Implement manual scroll restoration on page load

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
+});
+
 const $ = (id) => document.getElementById(id);
 let wedding = null;
 let galleryImages = [];
